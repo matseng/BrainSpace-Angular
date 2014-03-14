d6e6f682cb073cd199c5a137059b1a65735d7e98
Michael Tseng <matseng@gmail.com>
2014-03-14 09:32:33 -0700

    Able to submit notes to Firebase and improved css styling

--- app/modules/app.js
+++ app/modules/app.js
@@ -29,16 +29,46 @@ app.controller("app-controller", ['$scope', 'notesFactory',
   }
 ]);
 
-app.controller("noteSubmit-controller", ['$scope', 'notesFactory',
+// app.controller("noteSubmit-controller", ['$scope', 'notesFactory',
+//   function($scope, notesFactory){
+//     $scope.note = {username: "anonymous"};
+//     $scope.create = function(){
+//       var note = {
+//         title: $scope.note.title, 
+//         username: $scope.note.username
+//       };
+//     notesFactory.addNote(note);
+//     }
+
+//   }
+// ]);
+
+app.controller("noteContainer-controller", ['$scope', 'notesFactory',
   function($scope, notesFactory){
-    $scope.note = {username: "anonymous"};
+    // $scope.spice = "Hola Jalepenos";
+    // $scope.note;
     $scope.create = function(){
+      if(! $scope.note){
+        $scope.note = {};
+        $scope.note.title = 'no title yet';
+        $scope.note.body = 'no text yet';
+      }
       var note = {
-        title: $scope.note.title, 
-        username: $scope.note.username
-      };
-    notesFactory.addNote(note);
-    }
+        title: $scope.note.title,
+        body: $scope.note.body
+      }
+      notesFactory.addNote(note);
+    };
+    $scope.deleteButtonClicked = function(){
+      if(confirm("Confirm delete?")){
+        var elClicked = event.srcElement;
+        var noteContainer = elClicked.parentNode.parentNode;
+        var detached = noteContainer.remove();
+      }
+    };
+    $scope.copyButtonClicked = function(){
+      alert('Copy button clicked');
+    };
   }
 ]);
 
@@ -53,12 +83,58 @@ app.directive('addNoteListener', ['$document',
       link: link
     };
     function link(scope, element, attrs){
-      element.on('click', function(){
-        var x = mouse.clientX;
-        var y = mouse.clientY;
-      //TODO: add new note_template.html to x,y location
-        // note_template will have it's own directive
-        // How will scope data be handle by controller -> factory?
+      
+
+      element.on('click', function(mouse){
+        var elementClickedId = mouse.srcElement.id;
+        //SAVE:
+        // var menuButtonCheckedId = angular.element("#menuContainer .btn-group :checked").attr('id');
+        // if( elementClickedId === 'allNotesContainer' && menuButtonCheckedId === 'newNote') {
+        if( elementClickedId === 'allNotesContainer'){
+          var x = mouse.offsetX;
+          var y = mouse.offsetY;
+          var noteContainer = angular.element(document.getElementById('noteTemplate').content.cloneNode(true).children[0]);
+          element.append(noteContainer);
+          noteContainer.css({'position': 'absolute', 'left': x, 'top': y}); //NOTE: Can only modify certain CSS after the element is appended to the DOM
+          // angular.element('.textContent').focus();
+          // angular.element(('.textContent').on('focusout', function(){
+          //   $(this).attr('disabled', true);  //textarea is no longer editable - user will need to click edit
+          //   console.log('Will "autosave" in the future');
+          // }));
+        }
+      })
+    }
+  }
+]);
+
+/*
+- Listens for mouse click to add new notes at a specific position
+- This directive is added to #allNotesContainer element
+*/
+app.directive('addNoteListener_OLD', ['$document', 
+  function($document){
+    return {
+      restrict: 'A',
+      link: link
+    };
+    function link(scope, element, attrs){
+      element.on('click', function(mouse){
+        var elementClickedId = mouse.srcElement.id;
+        //SAVE:
+        // var menuButtonCheckedId = angular.element("#menuContainer .btn-group :checked").attr('id');
+        // if( elementClickedId === 'allNotesContainer' && menuButtonCheckedId === 'newNote') {
+        if( elementClickedId === 'allNotesContainer'){
+          var x = mouse.offsetX;
+          var y = mouse.offsetY;
+          var noteContainer = angular.element(document.getElementById('noteTemplate').content.cloneNode(true).children[0]);
+          element.append(noteContainer);
+          noteContainer.css({'position': 'absolute', 'left': x, 'top': y}); //NOTE: Can only modify certain CSS after the element is appended to the DOM
+          // angular.element('.textContent').focus();
+          // angular.element(('.textContent').on('focusout', function(){
+          //   $(this).attr('disabled', true);  //textarea is no longer editable - user will need to click edit
+          //   console.log('Will "autosave" in the future');
+          // }));
+        }
       })
     }
   }
