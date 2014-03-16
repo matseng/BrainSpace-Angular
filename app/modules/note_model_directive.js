d5d2814969b4de55bef3e3392b42793a04e812a6
Michael Tseng <matseng@gmail.com>
2014-03-16 13:34:58 -0700

    Able to change / save / load different textarea dimensions

--- app/modules/note_model_directive.js
+++ app/modules/note_model_directive.js
@@ -10,5 +10,26 @@ angular.module('BrainSpace')
       element.on('focusout', function(){
         $scope.$emit('updateNote', $scope);
       });
+      element.on('mouseup', function(){
+        console.log(element, attrs);
+        var width = element.find('textarea').css('width');
+        var height = element.find('textarea').css('height');
+        var body_style = {width: width, height: height};
+        $scope.note.body_style = body_style;
+        $scope.$emit('updateNote', $scope);
+      });
     }
-  }]);
+  }
+]);
+
+// angular.moduel('BrainSpace')
+//   .directive('resizeNotearea', [function(){
+//     return {
+//       restrict: 'A',
+//       link: function($scope, element, attrs){
+//         element.on()
+//           console.log($scope, element, attrs);
+//       }
+//     }
+//   }
+// ]);
