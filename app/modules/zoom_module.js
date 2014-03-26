94dce8fb8a77cbe718de2f486e2f79f642e70372
Michael Tseng <matseng@gmail.com>
2014-03-25 18:09:41 -0700

    Zoom directive loaded into index.html via a Zoom module

--- app/modules/zoom_module.js
+++ app/modules/zoom_module.js
@@ -1,12 +1,12 @@
 // zoom_module.js
 
-angular.module('zoom_module', []);
-  // .directive('zoom_directive', [function(){
-  //   return {
-  //     restrict: 'A',
-  //     link: link
-  //   };
-  //   function link(){
-  //     debugger;
-  //   };
-  // }]);
+angular.module('zoom_module', [])
+  .directive('zoomDirective', [function(){
+    return {
+      restrict: 'A',
+      link: link
+    };
+    function link(){
+      // debugger;
+    };
+  }]);
