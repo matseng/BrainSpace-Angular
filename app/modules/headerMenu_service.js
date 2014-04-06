f5e98410a012f7cbb18195364330bdb62ff3070d
Michael Tseng <matseng@gmail.com>
2014-04-06 13:36:43 -0700

    Change name of note_menu_service to headerMenu_service.js

---
+++ app/modules/headerMenu_service.js
@@ -0,0 +1,34 @@
+// headerMenu_service.js
+
+angular.module('BrainSpace').service('headerMenu_service', [function(){
+  var scope = {};
+  var element;
+  var fontSizeString;
+  var menuState;
+  
+  this.setScope = function(myScope) {
+    scope = myScope;
+    if(scope.note && scope.note.textarea_style){
+      if(scope.note.textarea_style.fontSize)
+        fontSizeString = scope.note.textarea_style.fontSize;
+    }
+  };
+  
+  this.getScope = function() {
+    return scope;
+  };
+
+  this.getFontSizeString = function() {
+    console.log('headerMenu_service, ', fontSizeString);
+    return fontSizeString;
+  };
+
+  this.setRadioButtonState = function(state) { 
+    menuState = state;
+  };
+
+  this.getRadioButtonState = function() {
+    return menuState;
+  };
+
+}]);
