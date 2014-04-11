8c1b8483168ef3b1a82a599ddc9ee8f03b45f711
Michael Tseng <matseng@gmail.com>
2014-04-10 18:58:54 -0700

    Moved noteMenu_directive.js to headerMenu folder.

---
+++ app/modules/headerMenu/noteMenu_directive.js
@@ -0,0 +1,30 @@
+// note_menu_directive.js
+
+angular.module('headerMenu_module')
+  .directive('noteMenuDirective', ['$rootScope', 'headerMenu_service', '$document', function($rootScope, headerMenu_service, $document){
+    return {
+      restrict: "E",
+      link: link
+    };
+
+    function link ($scope, element, attrs) {
+      element.bind('change', function(){
+        var selectedScope = headerMenu_service.getScope();
+        if(selectedScope){
+          var fontSize = event.srcElement.value;
+          var noteScope = $rootScope.noteSelected.scope;
+          var textareaElement = $rootScope.noteSelected.element.find('textarea');
+          noteScope.note.textarea_style = {'fontSize': fontSize};
+          textareaElement.attr('style', 'font-size: ' + fontSize);
+          // $scope.$emit('updateNote', $scope);  //message NOT recieved by child scope
+        }
+      });
+      // var selectedScope = headerMenu_service.getScope();
+      // selectedScope.$watch("$id", function(){
+      //   console.log(selectedScope.$id);
+      // })
+      // $document.getElementsByClassName('selectFontSize')[0].value = selectedScope.note.textarea_style.fontSize;​​​​​​​​​​
+
+    }
+  }
+]);
