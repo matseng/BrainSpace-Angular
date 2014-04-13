3ddbf2e6f3f27fc189afbfc10f7c4309b2470812
Michael Tseng <matseng@gmail.com>
2014-04-13 12:57:31 -0700

    Minor syntax cleanup of commented out code

--- app/modules/app.js
+++ app/modules/app.js
@@ -4,39 +4,6 @@
 
 var app = angular.module("BrainSpace", ['notes_factory_module', 'note_module', 'headerMenu_module', 'navigation_module', 'group_module']);
 
-// app.controller('menu-controller', ['$rootScope', '$scope', 'notesFactory', 'headerMenu_service',
-//   function($rootScope, $scope, notesFactory, headerMenu_service){
-//     $scope.buttonSelected = 'null';
-//     $scope.menuState = 'explore'; //explore, newNote, drawGroup
-//     $rootScope.menuState = $scope.menuState;
-
-//     $scope.$watch('menuState', function(newState){
-//       $rootScope.menuState = newState;
-//       headerMenu_service.setRadioButtonState($scope.menuState);
-//     });
-    
-//     $scope.fontSizeString = "10pt";
-//     $scope.$on('scope.update', function() {
-//     // $scope.$on('noteClicked', function() {
-//       //TODO: DOM view is not updating font size unless an element dragged
-//       $scope.fontSizeString = headerMenu_service.getFontSizeString();
-//       // $scope.$apply();  //trying to get DOM to update without needing to drag the element
-//       // console.log('in menu-controller: ', $scope.fontSizeString);
-//       // console.log(typeof $scope.fontSizeString);
-//     });
-
-//     $scope.deleteButtonClicked = function(event){
-//       if($rootScope.noteSelected){
-//         if(confirm("Confirm delete?")){
-//           notesFactory.deleteNote($rootScope.noteSelected.scope.key);
-//         }
-//       } else {
-//         console.log("Select a note prior to clicking delete.");
-//       }
-//     };
-//   }
-// ]);
-
 app.controller("allNotes_controller", ['$scope', '$firebase', 'notesFactory', 'headerMenu_service',
   function($scope, $firebase, notesFactory, headerMenu_service){
 
@@ -66,10 +33,6 @@ app.controller("allNotes_controller", ['$scope', '$firebase', 'notesFactory', 'h
       noteScope.note[propertyName] = updatedProperty[propertyName];
       $scope.notes.$save(noteScope.key);
     });
-
-    // $scope.$on('deleteNote', function(event, key) {
-    //   $scope.notes.$remove(key);
-    // });
 
     $scope.$on('addGroup', function(event, groupObject) {
       notesFactory.addGroup(groupObject);
@@ -102,10 +65,6 @@ app.controller("allNotes_controller", ['$scope', '$firebase', 'notesFactory', 'h
         var groupBottom = position.top + groupScope.group.height;
         for(var i = 0; i < noteKeysInGroup.length; i++) {
           note = $scope.notes[noteKeysInGroup[i]];
-          var deltaX = position.left - groupScope.group.left;
-          var deltaY = position.top - groupScope.group.top;
-          // note.position.left += deltaX;
-          // note.position.top += deltaY;          
           note.position.left = noteInitialPositions[i].left + deltaObj.deltaX;
           note.position.top = noteInitialPositions[i].top + deltaObj.deltaY;
         }
@@ -123,18 +82,3 @@ app.controller("allNotes_controller", ['$scope', '$firebase', 'notesFactory', 'h
     });
   }
 ]);
-
-// app.controller("note-controller", ['$scope',
-//   function($scope){
-//     $scope.fontSize = "12pt";
-
-//     $scope.change = function() {
-//       $scope.$emit('update:note', $scope);
-//     };
-
-//     // $scope.click = function() {
-//     //   //TODO: refactor from note_model_directive
-//     // };
-//   }
-// ]);
-
