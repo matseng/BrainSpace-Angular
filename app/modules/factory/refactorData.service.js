f3d14680d318c8510105f74d64027af260b586fc
Michael Tseng <matseng@gmail.com>
2014-04-17 09:15:25 -0700

    Refactored note model to include reference to parent group

--- app/modules/factory/refactorData.service.js
+++ app/modules/factory/refactorData.service.js
@@ -7,7 +7,34 @@ angular.module('notes_factory_module')
       var notes = notesFactory.getNotes();
       var keys = groups.$getIndex();
       console.log("count: " + keys.length);
+      
+      // var newGroupsCollection = {}; 
+      // var Group2 = function() {
+      //   this = {
+      //     data: {
+      //       parentGroup: null,
+      //       childNotes: null,
+      //       childGroups: null
+      //     },
+      //     style: null
+      //   };
+      // };
 
+      var newNotesCollection = {};
+      var Note2 = function(note, parentGroupKey) {
+        this.data = {
+          text: note.body,
+          parentGroup: parentGroupKey,
+          left: note.position.left,
+          top: note.position.top,
+          width: note.dimensions ? note.dimensions.width : 192,
+          height: note.dimensions ? note.dimensions.height : 100
+        };
+        this.style = {
+          'font-size': note.textarea_style ? note.textarea_style.fontSize : "10pt"
+        };
+      }
+      
       var deferred1 = $q.defer();
       groups.$on('loaded', function(groups){
         // var keys = groups.$getIndex();
@@ -23,10 +50,10 @@ angular.module('notes_factory_module')
       notes.$on('loaded', function(notes){
         // var keys = notes.$getIndex();
         deferred2.resolve(notes);
-        //console.log("count: " + keys.length);
+        console.log(Object.keys(notes).length);
       });
 
-      var getNearestAncestorKey = function(note, groups) {
+      var getNearestAncestorKey = function(note, key, groups) {
         var parentGroupKey = null;
         var minDist = null;
         var currDist;
@@ -45,8 +72,11 @@ angular.module('notes_factory_module')
             }
           }
         });
-        // console.log(minDist, parentGroupKey);
-        return parentGroupKey;
+        var note2 = new Note2(note, parentGroupKey);
+        newNotesCollection[key] = note2;
+        //console.log(note2);
+        //TODO: For each non-null parentGroupKey, create a new Group2 and add the current keyNote as a child
+        // return parentGroupKey;
       };
 
       var promiseAll = $q.all([deferred1.promise, deferred2.promise]);  ////Why can't a deferred instance include its own promise (ie what's the benefit of this line in general)
@@ -54,9 +84,10 @@ angular.module('notes_factory_module')
         var groups = response[0];
         var notes = response[1];
 
-        angular.forEach(notes, function(note, keyNote) {
-          console.log(getNearestAncestorKey(note, groups));
+        angular.forEach(notes, function(note, key) {
+          getNearestAncestorKey(note, key, groups);
         });
+        console.log(newNotesCollection, Object.keys(newNotesCollection).length);
 
         //TODO: Iterate over noteKeys --> create notes2 object with flat structure and save in Firebase
         //Find parent group of each note
