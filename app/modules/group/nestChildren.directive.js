1cb5151008dbd6bb8f2b9abc3eef8cb5a8b7f6fc
Michael Tseng <matseng@gmail.com>
2014-04-27 11:55:57 -0700

    On mouseup after dragging a group, save new global coordinates (x,y) of each note

--- app/modules/group/nestChildren.directive.js
+++ app/modules/group/nestChildren.directive.js
@@ -2,79 +2,93 @@
 // TODO: On mouse up, calculate and $save the notes' global x,y and style left,top
 
 angular.module('group_module')
-  .directive('nestChildrenDirective', ['notesFactory', '$compile', function(notesFactory, $compile) { 
-    return {
-      attribute: "A",
-      link: link
-    };
-
-
-    function link($scope, $element, attrs) {
-      var notes = notesFactory.getNotes2();
-      var groups = notesFactory.getGroups2();
-      var noteKeysInGroup;
-      var groupKeysInGroup;
-      var noteInitialPositions;
-      var groupInitialPositions;
-
-      var nestChildrenInDOM = function(noteKeysInGroup) {
-        // console.log($element, noteKeysInGroup);
-        var $childrenNotesContainer;
-        if(noteKeysInGroup.length > 0) {
-          $childrenNotesContainer = angular.element($element[0].getElementsByClassName('childrenNotes')[0]);
-          if($childrenNotesContainer.length == 0){
-            childrenNotesContainer = "<div class='childrenNotes'></div>";
-            $childrenNotesContainer = angular.element(childrenNotesContainer);
-            $element.append($childrenNotesContainer);
-          }
-          for(var i = 0; i < noteKeysInGroup.length; i++) {
-            var childEl = document.getElementById(noteKeysInGroup[i]);
-            var $childEl = angular.element(childEl);
-            var scope = $childEl.scope();
-            console.log($scope.group.data.x, $scope.group.data.y);
-            console.log($scope.group.style.left, $scope.group.style.top);
-            console.log(scope.note.style.left, scope.note.style.top);
-            scope.note.style.left = scope.note.style.left - $scope.group.style.left;  //NOTE: will likely cause errors in the future when data.x,y and style.top,left are fixed
-            scope.note.style.top = scope.note.style.top - $scope.group.style.top;
-            // scope.note.style.left = 0;
-            // scope.note.style.top = 0;
-            $childEl.css({'left': scope.note.style.left, 'top': scope.note.style.top })
-            $childrenNotesContainer.append($childEl);
-            $compile($childEl)(scope);
-            console.log($childEl.scope().note.data.text, noteKeysInGroup[i], $element);
-          }
-        }
-
+  .directive('nestChildrenDirective', ['notesFactory', '$compile', 'notesFactory', 
+    function(notesFactory, $compile, notesFactory) { 
+      return {
+        attribute: "A",
+        link: link
       };
 
-      $element.bind('mousedown', function() {
-        noteKeysInGroup = [];
-        noteInitialPositions = [];
-        var groupRight = $scope.group.style.left + $scope.group.style.width;
-        var groupBottom = $scope.group.style.top + $scope.group.style.height;
-        notesFactory.forEach(notes, function(note, key){
-          if(note.style && $scope.group.style.left <= note.style.left && note.style.left <= groupRight
-            && $scope.group.style.top <= note.style.top && note.style.top <= groupBottom) {
-            noteKeysInGroup.push(key);
-            noteInitialPositions.push({left: note.style.left, top: note.style.top});
+
+      function link($scope, $element, attrs) {
+        var notes = notesFactory.getNotes2();
+        var groups = notesFactory.getGroups2();
+        var noteKeysInGroup;
+        var groupKeysInGroup, groupKeysInGroupPrevious;
+        var noteInitialPositions;
+        var groupInitialPositions;
+        var groupInitialX, groupInitialY;
+        var deltaX, deltaY;
+
+        var nestChildrenInDOM = function(noteKeysInGroup) {
+          var $childrenNotesContainer;
+          var newLeft, newTop;
+          if(noteKeysInGroup.length > 0) {
+            $childrenNotesContainer = angular.element($element[0].getElementsByClassName('childrenNotes')[0]);
+            if($childrenNotesContainer.length == 0){
+              childrenNotesContainer = "<div class='childrenNotes'></div>";
+              $childrenNotesContainer = angular.element(childrenNotesContainer);
+              $element.append($childrenNotesContainer);
+            }
+            for(var i = 0; i < noteKeysInGroup.length; i++) {
+              var childEl = document.getElementById(noteKeysInGroup[i]);
+              var $childEl = angular.element(childEl);
+              var scope = $childEl.scope();
+              scope.note.style.left = scope.note.data.x - $scope.group.data.x;
+              scope.note.style.top = scope.note.data.y - $scope.group.data.y;
+              $childEl.css({'left': scope.note.style.left, 'top': scope.note.style.top })
+              $childrenNotesContainer.append($childEl);
+              $compile($childEl)(scope);
+            }
           }
+
+        };
+
+        $element.bind('mousedown', function() {
+          noteKeysInGroup = [];
+          // noteInitialPositions = [];
+          var groupRight = $scope.group.data.x + $scope.group.style.width;
+          var groupBottom = $scope.group.data.y + $scope.group.style.height;
+          groupInitialX = $scope.group.data.x;
+          groupInitialY = $scope.group.data.y;
+          notesFactory.forEach(notes, function(note, key){
+            if(note.data && $scope.group.data.x <= note.data.x && note.data.x <= groupRight
+              && $scope.group.data.y <= note.data.y && note.data.y <= groupBottom) {
+              noteKeysInGroup.push(key);
+              // noteInitialPositions.push({left: note.data.x, top: note.data.y});
+            }
+          });
+          
+          nestChildrenInDOM(noteKeysInGroup);
+
+          $element.bind('mouseup', myMouseUp);
+
         });
-        
-        nestChildrenInDOM(noteKeysInGroup);
 
-        // angular.forEach(notes, function(note, key) {
-        //   if($scope.group.left <= note.style.left && note.style.left <= groupRight
-        //     && $scope.group.top <= note.style.top && note.style.top <= groupBottom) {
-        //     noteKeysInGroup.push(key);
-        //     noteInitialPositions.push({left: note.style.left, top: note.style.top});
-        //   }
-        //   console.log(noteKeysInGroup);
-        // });
+        function myMouseUp(event) {
+          console.log(event);
+          var key;
+          var allNotesScope = notesFactory.getScope();
+          var note;
+          deltaX = $scope.group.data.x - groupInitialX;
+          deltaY = $scope.group.data.y - groupInitialY;
+          for(var i = 0; i < noteKeysInGroup.length; i++) {
+            key = noteKeysInGroup[i];
+            note = allNotesScope[key];
+            note.data.x += deltaX;
+            note.data.y += deltaY;
+            // note.style.left += deltaX;
+            // note.style.top += deltaY;
+            console.log(note);
 
-      });
-    };
+          }
+          //iterate over notes in group that has been dragged
+            //get window coordinates for each note
+        };
 
-  }]);
+      };  //END link
+    }
+  ]);
 
 
       // $scope.$on('update:group:mousedown', function(event, fromFile) {
