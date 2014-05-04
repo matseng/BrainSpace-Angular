a40625ac2e42e09460c7333ab0c2f5c22a0eb414
Michael Tseng <matseng@gmail.com>
2014-05-03 18:24:05 -0700

    Added TODO for using 'g' flag to search entire string

--- app/modules/note/noteModel_directive.js
+++ app/modules/note/noteModel_directive.js
@@ -14,6 +14,7 @@ app.directive('noteModelDirective', ['$rootScope', 'headerMenu_service', functio
     function link($scope, element, attrs) {
       var hashTags;
       element.on('focusout', function() {
+        // hashTags = $scope.note.data.text.match(/\#\w*/g) || null;  //TODO: use this line for multiple hashtags
         hashTags = $scope.note.data.text.match(/\#\w*/) || null;
         $scope.note.data.hashTags = hashTags;  //Ok to set to null, which overwrites hashTags that were just deleted by a user
         if(hashTags) {
