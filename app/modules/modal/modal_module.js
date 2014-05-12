05870fbb15596d8cf1efe61171af78338279e2a7
Michael Tseng <matseng@gmail.com>
2014-05-12 14:20:06 -0700

    Refactored modal DOM manipulation to modal_service

--- app/modules/modal/modal_module.js
+++ app/modules/modal/modal_module.js
@@ -1,57 +1,52 @@
 angular.module('modal_module', []);
 
 angular.module('modal_module')
-  .controller('modal_controller', ['$scope', function($scope) {
+  .controller('modal_controller', ['$scope', 'modal_service', function($scope, modal_service) {
     $scope.text = 'hello, hello world';
     
     $scope.$on('modal:update', function(event, fromFile, word) {
-      modalVisible();
+      modal_service.visible();
       $scope.text = word;
       $scope.$apply();
       console.log(word);
     });
 
-    var modalVisible = function() {
-      var $modal = angular.element(document.getElementById('modalBackground'));
-      $modal.css({visibility: 'visible'});
-    };
-
-    var toggleModal = function() {
-      var $modal = angular.element(document.getElementById('modalBackground'));
-      if(!$modal.css('visibility') || $modal.css('visibility') === 'hidden') {
-        $modal.css({visibility: 'visible'});
-      } else {
-        $modal.css({visibility: 'hidden'});
-      }
-    };
-
   }]);
 
   angular.module('modal_module')
-  .directive('modalDirective', [function() {
+  .directive('modalDirective', ['modal_service', function(modal_service) {
     return {
       restrict: 'A',
-      // controller: myController,
-      // templateURL: myTemplateURLFilename,
       link: link
-      // controller: function($scope, $element){}
     };
 
     function link($scope, $element, attrs) {
       $element.on('click', function(event) {
-        if(event.srcElement.classList.contains('modalBackground')) {
-          if(!$element.css('visibility') || $element.css('visibility') === 'hidden') {
-            $element.css({visibility: 'visible'});
-          } else {
-            $element.css({visibility: 'hidden'});
-          }
+        if(event.srcElement.id === 'modalBackground') {
+          modal_service.hidden();
         }
       });
     }
   }]);
 
-  // angular.module('modal_module')
-  // .service('modal_service', [function() {
-  //   var $modal = 
+  angular.module('modal_module')
+    .service('modal_service', [function() {
+      var $modal = angular.element(document.getElementById('modalBackground'));
 
-  // }]);
+      this.visible = function() {
+        $modal.css({visibility: 'visible'});
+      };
+
+      this.hidden = function() {
+        $modal.css({visibility: 'hidden'});
+      };
+
+      this.toggle = function() {
+        if(!$modal.css('visibility') || $modal.css('visibility') === 'hidden') {
+          $modal.css({visibility: 'visible'});
+        } else {
+          $modal.css({visibility: 'hidden'});
+        }
+      };
+
+  }]);
