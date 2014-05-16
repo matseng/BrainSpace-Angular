bb8ae0f007495c7883d8ffcec32065e831f9ef97
Michael Tseng <matseng@gmail.com>
2014-05-16 13:16:29 -0700

    Able to save pasted image data in firebase. Next is to render image upon loading

--- app/modules/nav/navigation_service.js
+++ app/modules/nav/navigation_service.js
@@ -30,6 +30,18 @@ angular.module('navigation_module')
       var str = "-webkit-transform: matrix(" + scale + ", 0, 0, " + scale + ", " + t.tx + ', ' + t.ty +');';
       // var str = '-webkit-transform: scale(' + scale + ',' + scale + ');';
       return str;
-    }
+    };
+
+    this.getGlobalCoordinates = function(windowX, windowY) {
+      var w = window,
+      d = document,
+      e = d.documentElement,
+      g = d.getElementsByTagName('body')[0],
+      windowX = windowX || w.innerWidth || e.clientWidth || g.clientWidth,
+      windowY = windowY || w.innerHeight|| e.clientHeight|| g.clientHeight;
+      var globalX = (windowX / 2 - t.tx) * (1 / scale);
+      var globalY = (windowY / 2 - t.ty) * (1 / scale);
+      //TODO...
+    };
 
   }]);
--- app/modules/note/note_module.js
+++ app/modules/note/note_module.js
@@ -1,11 +1,11 @@
 angular.module('note_module', []);
 
 angular.module('note_module')
-  .service('note_service', [function() {
+  .service('note_service', ['data_service', function(data_service) {
 
     var Note = function() {
       this.data = {
-        text: '',
+        text: 'Testing add image',
         parentGroup: [],
         x: 0,
         y: 0,
@@ -14,20 +14,28 @@ angular.module('note_module')
       this.style = {
         left: 0,
         top: 0,
-        width: 0,
-        height: 0,
-        'font-size': 0
+        width: 192,
+        height: 50,
+        'font-size': '10pt'
       };
     };
 
+/*
+* //Below causes error in Firebase b/c this object's prototype is a function that can't be saved
     Note.prototype.setImage = function(filePayload) {
       this.data.image = filePayload;
     };
+*/
+
+  var setImage = function(note, filePayload) {
+    note.data.image = filePayload;
+  };
 
     this.addImage = function(filePayload) {
       var note = new Note();
-      note.setImage(filePayload);
+      setImage(note, filePayload);
       console.log(note);
+      data_service.addNote(note);
       //add note via data_service
         //then work on data_service --> noteModel_directive --> note_template to render the image by replacing the textarea with an image
       //give image left, right the coordinate of the mouse
